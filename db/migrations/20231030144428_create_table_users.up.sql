ff0699b6bd651a9fec081f0e32e704148bb5f067
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-12 07:35:35 +0700

    refactor: Update user migration

--- db/migrations/20231030144428_create_table_users.up.sql
+++ db/migrations/20231030144428_create_table_users.up.sql
@@ -1,10 +1,6 @@
-create table users
-(
-    id         varchar(100) not null,
-    name       varchar(100) not null,
-    password   varchar(100) not null,
-    token      varchar(100) null,
-    created_at bigint       not null,
-    updated_at bigint       not null,
-    primary key (id)
+CREATE TABLE Users (
+    ID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Created DATETIME NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    HashedPassword CHAR(60) NOT NULL
 ) engine = InnoDB;
